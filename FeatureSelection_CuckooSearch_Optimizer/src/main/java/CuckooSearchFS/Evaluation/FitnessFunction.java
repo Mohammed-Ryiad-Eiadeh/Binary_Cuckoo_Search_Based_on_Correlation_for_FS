0004411a893cdc2ebee2c4ef30ad2361bddda27f
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2023-07-11 22:02:03 +0300

    Update FitnessFunction.java

--- FeatureSelection_CuckooSearch_Optimizer/src/main/java/CuckooSearchFS/Evaluation/FitnessFunction.java
+++ FeatureSelection_CuckooSearch_Optimizer/src/main/java/CuckooSearchFS/Evaluation/FitnessFunction.java
@@ -1,5 +1,4 @@
-
-package CuckooSearchFS.Evaluation;
+package org.tribuo.classification.fs.wrapper.Evaluation;
 
 import com.oracle.labs.mlrg.olcut.util.Pair;
 import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
@@ -7,15 +6,10 @@ import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import org.apache.commons.math3.stat.correlation.SpearmansCorrelation;
 import org.tribuo.*;
 import org.tribuo.classification.Label;
-import org.tribuo.classification.ensemble.VotingCombiner;
 import org.tribuo.classification.evaluation.LabelEvaluation;
 import org.tribuo.classification.evaluation.LabelEvaluator;
-import org.tribuo.common.nearest.KNNModel;
-import org.tribuo.common.nearest.KNNTrainer;
 import org.tribuo.dataset.SelectedFeatureDataset;
 import org.tribuo.evaluation.CrossValidation;
-import org.tribuo.math.distance.L1Distance;
-import org.tribuo.math.neighbour.NeighboursQueryFactoryType;
 import org.tribuo.provenance.FeatureSetProvenance;
 
 import java.io.File;
@@ -24,24 +18,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * This interface includes the evaluation function of each solution
- */
-
 public class FitnessFunction {
+    private final Trainer<Label> trainer;
     static double[][] matrix = null;
     private Correlation_Id correlation_id;
 
     /**
-     * Default constructor of the fitness function utility
+     * This interface includes the evaluation function of each solution
      */
-    public FitnessFunction() { }
+    public enum Correlation_Id {
+        PearsonsCorrelation, SpearmansCorrelation, KendallsCorrelation
+    }
 
-     /**
+    /**
+     * Default constructor of the fitness function utility
+     * @param trainer The used trainer in the evaluation process
+     */
+    public FitnessFunction(Trainer<Label> trainer) {
+        this.trainer = trainer;
+    }
+
+    /**
      * Constructs the matrix of the entire data in order to make it suitable to compute the correlation
      * @param path The URL of the data as a .CSV file format to be converted to matrix
+     * @param trainer The used trainer in the evaluation process
+     * @param c_id The used correlation function in the evaluation process
      */
-    public FitnessFunction(String path, Correlation_Id c_id) {
+    public FitnessFunction(String path, Trainer<Label> trainer, Correlation_Id c_id) {
+        this.trainer = trainer;
         matrix = getMatrix(path);
         this.correlation_id = c_id;
     }
@@ -56,15 +60,16 @@ public class FitnessFunction {
      */
     public <T extends FeatureSelector<Label>> double EvaluateSolution(T optimizer, Dataset<Label> dataset, ImmutableFeatureMap Fmap, int... solution) {
         SelectedFeatureDataset<Label> selectedFeatureDataset = new SelectedFeatureDataset<>(dataset,getSFS(optimizer, dataset, Fmap, solution));
-        KNNTrainer<Label> KnnTrainer =  new KNNTrainer<Label>(1, new L1Distance(), Runtime.getRuntime().availableProcessors(), new VotingCombiner(), KNNModel.Backend.THREADPOOL, NeighboursQueryFactoryType.BRUTE_FORCE);
-        CrossValidation<Label, LabelEvaluation> crossValidation = new CrossValidation<>(KnnTrainer, selectedFeatureDataset, new LabelEvaluator(), 10);
+        CrossValidation<Label, LabelEvaluation> crossValidation = new CrossValidation<>(trainer, selectedFeatureDataset, new LabelEvaluator(), 10);
         double avgAccuracy = 0D;
         for (Pair<LabelEvaluation, Model<Label>> ACC : crossValidation.evaluate()) {
             avgAccuracy += ACC.getA().accuracy();
         }
         avgAccuracy /= crossValidation.getK();
         double correlation = 0;
-        if (matrix != null) {correlation = getCorrelation(correlation_id, solution);}
+        if (matrix != null) {
+            correlation = getCorrelation(correlation_id, solution);
+        }
 
         return avgAccuracy + 0.001 * (1 - ((double) selectedFeatureDataset.getSelectedFeatures().size() / Fmap.size()) - correlation);
     }
@@ -84,7 +89,7 @@ public class FitnessFunction {
             if (solution[i] == 1) {
                 names.add(Fmap.get(i).getName());
                 scores.add(1D);
-            } 
+            }
         }
         FeatureSetProvenance provenance = new FeatureSetProvenance(SelectedFeatureSet.class.getName(), dataset.getProvenance(), optimizer.getProvenance());
 
@@ -104,6 +109,7 @@ public class FitnessFunction {
             while (originalData.hasNext()) {
                 listOfRows.add(originalData.nextLine());
             }
+
             matrix = new double[listOfRows.size()][listOfRows.get(0).split(",").length - 1];
             for (int i = 0; i < listOfRows.size() - 1; i++) {
                 String[] breakLine = listOfRows.get(i).split(",");
@@ -126,16 +132,17 @@ public class FitnessFunction {
     private double getCorrelation(Correlation_Id correlation_id, int... solution) {
         List<Integer> indecies = new ArrayList<>();
         for (int index = 0; index < solution.length; index++) {
-            if (solution[index] == 1) {
-                indecies.add(index); 
-            }
+            if (solution[index] == 1)
+                indecies.add(index);
         }
+
         double[][] mat = new double[matrix.length][indecies.size() - 1];
         for (int r = 0; r < mat.length; r++) {
             for (int c = 0; c < mat[0].length; c++) {
-                mat[r][c] = matrix[r][indecies.get(c)]; 
+                mat[r][c] = matrix[r][indecies.get(c)];
             }
         }
+
         return switch (correlation_id) {
             case PearsonsCorrelation -> new PearsonsCorrelation().computeCorrelationMatrix(mat).getNorm() / mat[0].length;
             case SpearmansCorrelation -> new SpearmansCorrelation().computeCorrelationMatrix(mat).getNorm() / mat[0].length;
